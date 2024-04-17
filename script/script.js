4e0ec70f45303100b03b41105a7f5015959cbd01
Isabela Melleis <160625375+isabelamelleis@users.noreply.github.com>
2024-04-17 12:48:29 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function login() {
+    var nome = document.getElementById('nome').value;
+    var senha = document.getElementById('senha').value;
+    var nomeSenha = [nome, senha];
+    var usuario = [];
+    usuario.push(nomeSenha);
+    if 
+}
