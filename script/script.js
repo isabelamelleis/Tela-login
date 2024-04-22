d144935d84781dfd6967fc36b1d5dcd466f57480
Isabela Melleis <160625375+isabelamelleis@users.noreply.github.com>
2024-04-22 12:45:05 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,8 +1,18 @@
+var usuario = {
+    nome: "isabela",
+    senha: "1234"
+}
+
 function login() {
-    var nome = document.getElementById('nome').value;
-    var senha = document.getElementById('senha').value;
-    var nomeSenha = [nome, senha];
-    var usuario = [];
-    usuario.push(nomeSenha);
-    if 
+    let nome = document.getElementById('nome').value; //pega o input do html e coloca na variável nome
+    let senha = document.getElementById('senha').value;
+    if (nome == usuario.nome && senha == usuario.senha) {
+        /*var mostrarNome = document.getElementById("nomeUsuario");
+        var mostrarSenha = document.getElementById("senhaUsuario");
+        mostrarNome.innerHTML = usuario.nome;
+        mostrarSenha.innerHTML = usuario.senha;*/
+        location.replace ("mostrarLogin.html");  // "location.replace" faz com que mude de página. A diferença entre ele e "window.location.href" é que com o location.replace o usuário não consegue voltar para a tela anterior usando o botão de voltar.
+    } else {
+        alert("Usuário ou senha incorretos.");
+    }
 }
