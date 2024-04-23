afaadc2e6b5f27fe81b1dd3b4d3eee91bcaf0caf
Isabela Melleis <160625375+isabelamelleis@users.noreply.github.com>
2024-04-23 18:45:07 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -4,10 +4,10 @@ var usuario = {
 }
 
 function login() {
-    let nome = document.getElementById('nome').value; // Atribui o conteúdo do input na variável "nome".
+    let nome = document.getElementById('nome').value; // Atribui o conteúdo do input na variável "nome"
     let senha = document.getElementById('senha').value;
-    if (nome == usuario.nome && senha == usuario.senha) { // Se a sentença for correta, redireciona o usuário para outra página. 
-        location.replace ("mostrarLogin.html");  // "location.replace" faz com que mude de página. A diferença entre ele e "window.location.href" é que com o location.replace o usuário não consegue voltar para a tela anterior usando o botão de voltar.
+    if (nome == usuario.nome && senha == usuario.senha) { // Se a sentença for correta, redireciona o usuário para outra página
+        location.replace ("mostrarLogin.html");  // "location.replace" faz com que mude de página. A diferença entre ele e "window.location.href" é que com o location.replace o usuário não consegue voltar para a tela anterior usando o botão de voltar
     } else if (nome !== usuario.nome) {
         alert("Nome de usuário incorreto.");
     } else {
